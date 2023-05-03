f736311cac16215d3517151ea93fc15a278fe8e7
dolpvv57 <dolpvv@qq.com>
2023-05-03 17:26:02 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,18 @@
 module.exports = {
   themeConfig: {
     // logo: '/assets/img/logo.png',
-    base: '/docs/',
+    // base: '/docs/',
     lastUpdated: 'Last Updated',
     nav: [
       { text: 'Home', link: '/' },
       { text: 'About', link: '/about/' },
-      { 
-        text: 'Group',
-        items: [
-          { text: 'Group1', items: [/*  */] },
-          { text: 'Group2', items: [/*  */] }
-        ]
-      }
+      // { 
+      //   text: 'Group',
+      //   items: [
+      //     { text: 'Group1', items: [/*  */] },
+      //     { text: 'Group2', items: [/*  */] }
+      //   ]
+      // }
     ]
   }
 }
