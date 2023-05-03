c1a31eb1caa5694098f30546498e6221c4a59716
dolpvv57 <dolpvv@qq.com>
2023-05-03 13:18:15 +0800

    test

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  themeConfig: {
+    // logo: '/assets/img/logo.png',
+    lastUpdated: 'Last Updated',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about/' },
+      { 
+        text: 'Group',
+        items: [
+          { text: 'Group1', items: [/*  */] },
+          { text: 'Group2', items: [/*  */] }
+        ]
+      }
+    ]
+  }
+}
